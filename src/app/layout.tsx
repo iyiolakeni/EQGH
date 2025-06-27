cab2ccde885b47f451f645e37984d2e5defda335
iyiolakeni <iawe@polarisbanklimited.com>
2025-06-27 12:27:57 +0100

    Added Analytics to the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,34 +2,36 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer, Header } from "./navigation/nav";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "De Wura and Alfred Hotel",
-	description: "Experience Luxury Beyond Compare",
+  title: "De Wura and Alfred Hotel",
+  description: "Experience Luxury Beyond Compare",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
-				<Header />
-				{children}
-				<Footer />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
+        {children}
+        <Analytics />
+        <Footer />
+      </body>
+    </html>
+  );
 }
